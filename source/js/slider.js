61b34172a78128301d5df1c5a819050f5a2be393
dYAROSLAVy <yadsp@yandex.ru>
2024-02-13 22:31:03 +0500

    Исправляет слайдер

--- source/js/slider.js
+++ source/js/slider.js
@@ -5,6 +5,16 @@ const control = document.querySelector('.results__slider-control');
 
 let isActive = false;
 
+const sliderReset = () => {
+  isActive = false;
+  before.style.clipPath = `inset(0 50% 0 0)`;
+  after.style.clipPath = `inset(0 0 0 50%)`;
+  control.style.left = `50%`;
+  before.style.transition = '0.3s'
+  after.style.transition = '0.3s'
+  control.style.transition = '0.3s'
+};
+
 const sliderChange = (x) => {
   let shift = Math.max(0, Math.min(x, slider.offsetWidth));
   before.style.clipPath = `inset(0 ${slider.offsetWidth - shift}px 0 0)`;
@@ -20,8 +30,12 @@ const initSliderChange = (x) => {
 
 const onSliderMouseMove = (evt) => {
   let x = evt.pageX;
-
-  initSliderChange(x);
+  if (isActive) {
+    before.style.transition = 'none'
+    after.style.transition = 'none'
+    control.style.transition = 'none'
+    initSliderChange(x);
+  }
 };
 
 const onSliderTouchMove = (evt) => {
@@ -30,34 +44,34 @@ const onSliderTouchMove = (evt) => {
   for (let i = 0; i < evt.changedTouches.length; i++) {
     x = evt.changedTouches[i].pageX;
   }
-
+  before.style.transition = 'none'
+  after.style.transition = 'none'
+  control.style.transition = 'none'
   initSliderChange(x);
 };
 
 const desktopSlider = () => {
-  if(!control) {
-    return;
-  }
-
   control.addEventListener('mousedown', () => isActive = true);
   control.addEventListener('mouseup', () => isActive = false);
-  slider.addEventListener('selectstart', (evt) => evt.preventDefault())
-  slider.addEventListener('mouseleave', () => isActive = false);
+  control.addEventListener('selectstart', (evt) => evt.preventDefault())
+  slider.addEventListener('mouseleave', () => sliderReset());
   slider.addEventListener('mousemove', (evt) => onSliderMouseMove(evt));
 };
 
 const mobileSlider = () => {
-  if(!control) {
-    return;
-  }
-
   control.addEventListener('touchstart', () => isActive = true);
   control.addEventListener('touchend', () => isActive = false);
-  control.addEventListener('touchcancel', () => isActive = false);
-  control.addEventListener('touchmove', (evt) => onSliderTouchMove(evt));
+  slider.addEventListener('touchcancel', () => {
+    isActive = false;
+    sliderReset();
+  });
+  slider.addEventListener('touchmove', (evt) => onSliderTouchMove(evt));
 };
 
 const initSlider = () => {
+  if (!slider) {
+    return;
+  }
   mobileSlider();
   desktopSlider();
 };
